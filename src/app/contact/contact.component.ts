15e3ef91244c14356ab3c195a6e21cde0e4c0b30
pkieber <113725311+pkieber@users.noreply.github.com>
2023-05-21 22:24:23 +0200

    update contact form + add active links

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,38 +6,93 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  sent = false;
+
   @ViewChild('myForm') myForm!: ElementRef;
   @ViewChild('nameField') nameField!: ElementRef;
   @ViewChild('emailField') emailField!: ElementRef;
   @ViewChild('messageField') messageField!: ElementRef;
   @ViewChild('sendButton') sendButton!: ElementRef;
 
+
+  /**
+   * Sends the email by collecting the form data and sending it to the server.
+   */
   async sendMail() {
     let nameField = this.nameField.nativeElement;
     let emailField = this.emailField.nativeElement;
     let messageField = this.messageField.nativeElement;
     let sendButton = this.sendButton.nativeElement;
+    this.disableInputFields(nameField, emailField, messageField, sendButton);
+    let formData = new FormData();
+    this.appendFormData(formData, nameField, emailField, messageField);
+    await this.sendToServer(formData);
+    this.enableInputFields(nameField, emailField, messageField, sendButton);
+    this.resetInputFields(nameField, emailField, messageField);
+    setTimeout(() => {
+      this.sent = false;
+    }, 3000);
+  }
 
+
+  /**
+  * Disables input fields.
+  */
+  disableInputFields(nameField: any, emailField: any, messageField: any, sendButton: any){
     nameField.disabled = true;
     emailField.disabled = true;
     messageField.disabled = true;
     sendButton.disabled = true;
+  }
 
-    let fd = new FormData();
-    fd.append('name', nameField.value);
-    fd.append('email', nameField.value);
-    fd.append('message', messageField.value);
 
-    // send email
-    await fetch('https://patrick-kieber.developerakademie.net/send_mail/send_mail.php'),
-      {
-        method: 'POST',
-        body: fd
-      }
+  /**
+   * Appends the field values to the given FormData object.
+   * @param formData
+   * @param nameField
+   * @param emailField
+   * @param messageField
+   */
+  appendFormData(formData: any, nameField: any, emailField: any, messageField: any) {
+    formData.append('name', nameField.value);
+    formData.append('email', emailField.value);
+    formData.append('message', messageField.value);
+  }
 
+
+  /**
+   * Sends message to server.
+   * @param formData
+   */
+  async sendToServer(formData: any){
+    await fetch('https://patrick-kieber.developerakademie.net/send_mail/send_mail.php', {
+      method: 'POST',
+      body: formData
+    });
+    this.sent = true;
+  }
+
+
+  /**
+  * Enables input fields.
+  */
+  enableInputFields(nameField: any, emailField: any, messageField: any, sendButton: any){
     nameField.disabled = false;
     emailField.disabled = false;
     messageField.disabled = false;
     sendButton.disabled = false;
+  }
+
+
+  /**
+   * Resets all input fields.
+   * @param nameField
+   * @param emailField
+   * @param messageField
+   */
+  resetInputFields(nameField: any, emailField: any, messageField: any){
+    nameField.value = '';
+    emailField.value = '';
+    messageField.value = '';
   }
 }
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,14 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent {
+
+export class NavigationComponent implements OnInit {
+  currentSection: string | null = null;
   isNavOpen = false;
 
-  toggleNav() {
+  ngOnInit(): void {
+    this.onScroll();
+  }
+
+
+  /**
+   * Handles the scroll event and updates the current active section.
+   * @param target The target element for scrolling. Defaults to `document.documentElement`.
+   */
+  @HostListener('window:scroll', ['$event.target'])
+  onScroll(target: HTMLElement = document.documentElement) {
+    const sections = target.querySelectorAll('section');
+    const scrollPosition = target.scrollTop || window.pageYOffset || target.scrollTop || 0;
+
+    for (let i = sections.length - 1; i >= 0; i--) {
+      const section = sections[i] as HTMLElement;
+      const sectionId = section.getAttribute('id');
+      const sectionOffset = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+
+      if (scrollPosition >= sectionOffset && scrollPosition < sectionOffset + sectionHeight) {
+        this.currentSection = sectionId;
+        break;
+      }
+    }
+  }
+
+
+  /**
+   * Sets the active section.
+   * @param sectionId The ID of the section to set as active.
+   */
+  setActiveSection(sectionId: string): void {
+    this.currentSection = sectionId;
+  }
+
+
+  /**
+   * Toggles the navigation menu.
+   */
+  toggleNav(): void {
     this.isNavOpen = !this.isNavOpen;
 
     if (this.isNavOpen) {
@@ -18,14 +60,24 @@ export class NavigationComponent {
     }
   }
 
-  openNav() {
-    const mobileNav = document.getElementById("mobileNav") as HTMLElement;
-    mobileNav.style.width = "100%";
+
+  /**
+   * Opens the navigation menu when in mobile mode.
+   * Sets the width of the mobile navigation menu to 100%.
+   */
+  openNav(): void {
+    const mobileNav = document.getElementById('mobileNav') as HTMLElement;
+    mobileNav.style.width = '100%';
   }
 
+
+  /**
+   * Closes the navigation menu.
+   * Sets the width of the mobile navigation menu to 0% and resets the `isNavOpen` flag.
+   */
   closeNav(): void {
-    const mobileNav = document.getElementById("mobileNav") as HTMLElement;
-    mobileNav.style.width = "0%";
+    const mobileNav = document.getElementById('mobileNav') as HTMLElement;
+    mobileNav.style.width = '0%';
     this.isNavOpen = false;
   }
 }
